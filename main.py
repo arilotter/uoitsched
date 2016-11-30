17f1913437651dce43fda9d0f435c710a4e24e55
arilotter <arilotter@gmail.com>
2016-11-30 14:03:43 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from schedule import get_schedule
+from datetime import datetime
+from base64 import b64encode
+from urllib import parse
+
+# calendar = get_schedule(USERNAME, PASSWORD, start_date)
+# with open(USERNAME + '.ics', 'w') as cal_file:
+#       cal_file.writelines(calendar)
+
+
+# Winter semester, 2017
+start_date = datetime(2017, 1, 9)
+
+class Server(BaseHTTPRequestHandler):
+  def _set_headers(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+
+  def do_GET(self):
+    self._set_headers()
+    with open('index.html', 'rb') as f:
+      self.wfile.write(f.read())
+
+  def do_HEAD(self):
+    self._set_headers()
+      
+  def do_POST(self):
+    self.send_response(200)
+    content_length = int(self.headers['Content-Length'])
+    post_data = self.rfile.read(content_length)
+    post_data = {k:v for k, v in (x.split('=') for x in post_data.decode('utf-8').split('&'))}
+
+    schedule, warnings = get_schedule(post_data['username'], parse.unquote(post_data['password']), start_date)
+    if schedule:
+      self.send_response(200)
+      self.send_header('Content-Type', 'text/html')
+      self.end_headers()
+      encoded_schedule = 'data:application/octet-stream;base64,' + b64encode(str(schedule).encode('utf-8')).decode('utf-8')
+      output = '<html><body><h1>Schedule created '
+      if warnings:
+        output += 'with %s warning%s!</h1><ul>'% (len(warnings), '' if len(warnings) == 1 else 's')
+        for warning in warnings:
+          output += '<li>' + warning + '</li>'
+        output += '</ul>'
+      else:
+        output += 'successfully!</h1>'
+      output += '<h2><a download="%s" href="%s" title="Download Schedule">Click here to download your schedule</a></h2>' % (post_data['username'] + '.ics', encoded_schedule)
+      output += '<a href="/"">go back</a><p>Created by <a href="https://arilotter.com/">Ari Lotter</a></p></body></html>'
+
+      self.wfile.write(output.encode('utf-8'))
+      
+    else:
+      self.send_response(200)
+      self.send_header('Content-Type', 'text/html')
+      self.end_headers()
+      self.wfile.write(b'<html><body><h1>Bad username or password!</h1><a href="/"">go back</a><p>Created by <a href="https://arilotter.com/">Ari Lotter</a></p></body></html>')
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8080):
+  server_address = ('', port)
+  httpd = server_class(server_address, handler_class)
+  print('Starting httpd...')
+  httpd.serve_forever()
+
+if __name__ == "__main__":
+  run()
---
+++ schedule.py
@@ -0,0 +1,62 @@
+#! /usr/bin/python3
+from bs4 import BeautifulSoup
+from ics import Calendar, Event
+import requests
+from datetime import datetime, timedelta
+import weekday
+
+lookup_table = {}
+TIMEZONE_OFFSET = 5 # eastern standard time
+login_url = 'https://portal.mycampus.ca/cp/ip/login?sys=sct&url='
+
+def parse_schedule(soup):
+  # There is a single CRN tag associated with every course, so we can use it to find all the info for each course.
+  crn_tags = [ac.parent.findNext('td') for ac in soup.findAll('acronym')]
+
+  events = []
+  warnings = []
+
+  for crn in crn_tags:
+    # td < tr < table
+    meta_table = crn.parent.parent
+    course_title, course_code, course_section = meta_table.find('caption').string.split(' - ')
+
+    # Week, Type, Time, Days, Where, Date Range, Schedule Type, Instructors
+    times_table = [list(tr.findAll('td')) for tr in meta_table.findNext('table').findAll('tr')[1:]]
+    for row in times_table:
+      times, day, location, dates, kind, instructor = [td.string.replace('\xa0', '') if td.string else '' for td in row][2:]
+      try:
+        start_time, end_time = (datetime.strptime(time_string, "%I:%M %p") for time_string in times.split(' - '))
+        start_date, end_date = (datetime.strptime(date_string, "%b %d, %Y") for date_string in dates.split(' - '))
+      except ValueError:
+        warnings += ['Course does not have an assigned meeting time: ' + course_title + ' ' + kind]
+      class_dates = weekday.weekday_range(start_date, end_date, day)
+      for date in class_dates:
+        datetime_ = datetime(year=date.year, month=date.month, day=date.day)
+        start_datetime = datetime_ + timedelta(hours=start_time.hour + TIMEZONE_OFFSET, minutes=start_time.minute, seconds=start_time.second)
+        end_datetime = datetime_ + timedelta(hours=end_time.hour + TIMEZONE_OFFSET, minutes=end_time.minute, seconds=end_time.second)
+        event = Event(begin=start_datetime, end=end_datetime)
+        event.name = course_title + ' ' + kind
+        event.location = location.split()[-1]
+        event.description = 'CRN: %s\nCourse Code: %s\nSection: %s\nInstructor:%s\n' % (crn.string, course_code, course_section, instructor)
+        events += [event]
+
+  return events, warnings
+
+def get_schedule(username, password, start_date):
+  payload = {
+    'user': username,
+    'pass': password,
+    'uuid': '0xACA021'
+  }
+  with requests.Session() as request_session:
+    p = request_session.post('https://portal.mycampus.ca/cp/home/login', data=payload)
+    detail_url = 'https://ssbp.mycampus.ca/prod_uoit/bwskfshd.P_CrseSchdDetl'
+    request_session.get(login_url + detail_url)
+    r = request_session.post(detail_url, data={'term_in': start_date.strftime('%Y%m')})
+    soup = BeautifulSoup(r.text, 'html.parser')
+    if soup.find('title').string == 'User Login':
+      return False, []
+    events, warnings = parse_schedule(soup)
+    calendar = Calendar(events=events)
+    return calendar, warnings
